b99b4a999506f12ee6bbdb11c06f648454988444
andrewgustavo <andrew_gustavoo@hotmail.com>
2024-11-18 19:09:16 -0300

    Versão Estável do Projeto

---
+++ SoftCastStudioCreator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SoftCastStudioCreator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
---
+++ SoftCastStudioCreator/Views/DashboardPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace SoftCastStudioCreator.Views
+{
+    public partial class DashboardPage : ContentPage
+    {
+        public DashboardPage(string nomeCriador)
+        {
+            InitializeComponent();
+
+            // Certifique-se de que a Label BemVindoLabel foi corretamente encontrada
+            BemVindoLabel.Text = $"Bem-vindo, {nomeCriador}, a sua Dashboard SoftCast";
+        }
+
+        private async void OnLogoutClicked(object sender, EventArgs e)
+        {
+            // Navegar de volta para a página de login
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        // Evento para o botão "Incluir Lista de Conteúdo"
+        private async void OnIncluirListaClicked(object sender, EventArgs e)
+        {
+            // Lógica para incluir lista de conteúdo
+            //DisplayAlert("Incluir Lista", "Aqui você poderá incluir suas listas de conteúdo.", "OK");
+            await Navigation.PushAsync(new NewContentPage());
+        }
+
+        // Evento para o botão "Indicadores de Performance"
+        private void OnIndicadoresClicked(object sender, EventArgs e)
+        {
+            // Lógica para acessar indicadores de performance
+            DisplayAlert("Indicadores de Performance", "Aqui você pode visualizar os indicadores de performance.", "OK");
+        }
+    }
+}
